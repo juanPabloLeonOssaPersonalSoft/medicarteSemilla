e2dd39fd7e0885f792c5514d4f4653f722c37aa1
PERSONALSOFT\jocampillo <jhon.campillo@hotmail.com>
2018-04-02 09:52:37 -0500

    Primer cambio

--- src/test/java/aplicacion/serenity/pacientes/pages/PacientesPages.java
+++ src/test/java/aplicacion/serenity/pacientes/pages/PacientesPages.java
@@ -15,4 +15,36 @@ import org.openqa.selenium.Keys;
 
 
 public class PacientesPages extends PageObject {
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[9]/div[2]/ngl-virtual-select/a[1]/input")
+    WebElementFacade cmdRH;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[9]/div[2]/ngl-virtual-select/a[2]/input")
+    WebElementFacade txtRH;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[10]/div[2]/ngl-virtual-select/a[1]/input")
+    WebElementFacade cmdCodigoOcupacion;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[10]/div[2]/ngl-virtual-select/a[2]/input")
+    WebElementFacade txtCodigoOcupacion;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[11]/div[2]/p-calendar/span/div/div/div/select[1]")
+    WebElementFacade txtFechaNacimiento;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[12]/div[2]/input")
+    WebElementFacade txtEdad;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[13]/div[2]/ngl-virtual-select/a[1]/input")
+    WebElementFacade cmdEscolaridad;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[13]/div[2]/ngl-virtual-select/a[2]/input")
+    WebElementFacade txtEscolaridad;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[14]/div[2]/ngl-virtual-select/a[1]/input")
+    WebElementFacade cmdRegimen;
+
+    @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[14]/div[2]/ngl-virtual-select/a[2]/input")
+    WebElementFacade txtRegimen;
+
+
 }
