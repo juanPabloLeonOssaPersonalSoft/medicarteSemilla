407426f8b8b0fca986ac42e5750c5acf1a1b29f3
Test <elibustamante@personalsoft.com.co>
2018-04-02 10:15:08 -0500

    modificacion

--- src/test/java/aplicacion/serenity/pacientes/pages/PacientesPages.java
+++ src/test/java/aplicacion/serenity/pacientes/pages/PacientesPages.java
@@ -20,28 +20,6 @@ public class PacientesPages extends PageObject {
     @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[30]/div[2]/ngl-virtual-select/a[1]/input")
     WebElementFacade modeloAtencion;
 
-    public WebDriver driver;
-    private void waitEvent(int timeInMilliseconds){
-        try {
-            Thread.sleep(timeInMilliseconds*1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public PacientesPages(WebDriver driver) {
-        super(driver);
-        this.driver = driver;
-    }
-
-
-    public void setModeloAtencion(ExamplesTable data) {
-        modeloAtencion.click();
-        modeloAtencion.type(data.getRow(0).get("rol"));
-        modeloAtencion.sendKeys(Keys.ENTER);
-        waitEvent(1);
-    }
-
-
     @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[15]/div[2]/ngl-virtual-select/a[1]")
     WebElementFacade TipoAfiliacion;
 
@@ -74,6 +52,27 @@ public class PacientesPages extends PageObject {
 
     @FindBy(xpath = "//*[@id=\'simContainer\']/admin-patients/div[2]/div/div[2]/form/div[20]/div[2]/input")
     WebElementFacade txtTelefono;
+
+    public WebDriver driver;
+    private void waitEvent(int timeInMilliseconds){
+        try {
+            Thread.sleep(timeInMilliseconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public PacientesPages(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+    }
+
+
+    public void setModeloAtencion(ExamplesTable data) {
+        modeloAtencion.click();
+        modeloAtencion.type(data.getRow(0).get("rol"));
+        modeloAtencion.sendKeys(Keys.ENTER);
+        waitEvent(1);
+    }
 
     public void setTipoAfiliacion(ExamplesTable datos){
         esperar(2);
