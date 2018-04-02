43127db5d28523fd888a8cf329964eb67213e936
PERSONALSOFT\cerobledo <cerobledo@personalsoft.com.co>
2018-04-02 09:23:50 -0500

    creacion de la extructura pacientes pages, steps y definitions

---
+++ src/test/java/aplicacion/serenity/pacientes/definitions/PacientesDefinitions.java
@@ -0,0 +1,13 @@
+package aplicacion.serenity.pacientes.definitions;
+
+import aplicacion.serenity.pacientes.steps.PacientesSteps;
+import aplicacion.serenity.menu.steps.MenuPrimerNivelSteps;
+import aplicacion.serenity.menu.steps.MenuPrincipalSteps;
+import aplicacion.serenity.menu.steps.MenuSegundoNivelSteps;
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
+
+public class PacientesDefinitions {
+}
---
+++ src/test/java/aplicacion/serenity/pacientes/steps/PacientesSteps.java
@@ -0,0 +1,12 @@
+package aplicacion.serenity.pacientes.steps;
+
+import net.thucydides.core.steps.ScenarioSteps;
+import aplicacion.serenity.pacientes.pages.PacientesPages;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+import org.jbehave.core.model.ExamplesTable;
+
+public class PacientesSteps   extends ScenarioSteps {
+
+}
