8446069215dbdaf0d75e7bd0a412394607bde6c7
PERSONALSOFT\jocampillo <jhon.campillo@hotmail.com>
2018-04-06 10:35:24 -0500

    dgd

--- src/test/java/aplicacion/serenity/pacientes/steps/PacientesSteps.java
+++ src/test/java/aplicacion/serenity/pacientes/steps/PacientesSteps.java
@@ -24,7 +24,6 @@ public class PacientesSteps extends ScenarioSteps {
         pacientesPages.ingresarFechaNacimiento();
         pacientesPages.setClicCerrarTooltip();
         pacientesPages.estadoCivil(datos);
-
         pacientesPages.ingresarRH(datos);
         pacientesPages.ingresarCodigoOcupacion(datos);
         pacientesPages.ingresarEscolaridad(datos);
